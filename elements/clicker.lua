a00889017a89b437a37ab7300e3b57c448f880a2
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-24 00:17:09 +0530

    Added Clicker loading

--- elements/clicker.lua
+++ elements/clicker.lua
@@ -2,5 +2,13 @@ local clicker = {}
 
 
 function clicker:load()
+    self.img = love.graphics.newImage("assets/clicker.png")
+    self.width = self.img:getWidth()
+    self.height = self.img:getHeight()
     
+    self.scale = 1
+    self.currentWidth = self.width/self.scale
+    self.currentHeight = self.height/self.scale
+    
+    self.x = 
 end
