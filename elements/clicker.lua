6620bfb48b98ada5bf02e3e32bf3d8a5bd8de058
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-24 00:35:42 +0530

    Fixed the scaler

--- elements/clicker.lua
+++ elements/clicker.lua
@@ -1,34 +1,37 @@
 local clicker = {}
 
-
 function clicker:load()
     self.img = love.graphics.newImage("assets/clicker.png")
     self.width = self.img:getWidth()
     self.height = self.img:getHeight()
-    
+
     self.scale = 1
-    self.currentWidth = self.width/self.scale
-    self.currentHeight = self.height/self.scale
+    self.x = wW / 2
+    self.y = wH / 2
+end
 
-    self.x = wW/2 - self.currentWidth/2
-    self.y = wH/2 - self.currentHeight/2
- end
-
- function clicker:update(dt)
-    
+function clicker:update(dt)
     local mx, my = love.mouse.getPosition()
-    self.currentWidth = self.width/self.scale
-    self.currentHeight = self.height/self.scale
+    local scaledWidth = self.width * self.scale
+    local scaledHeight = self.height * self.scale
+    local drawX = self.x - scaledWidth / 2
+    local drawY = self.y - scaledHeight / 2
 
-    if mx > self.x and mx < self.x + self.currentWidth and my > self.y and my < self.y + self.currentHeight then
-        self.scale = 2
+    if mx > drawX and mx < drawX + scaledWidth and my > drawY and my < drawY + scaledHeight and love.mouse.isDown(1) then
+        self.scale = 1.2
     else
         self.scale = 1
     end
- end
+end
 
- function clicker:draw()
-    love.graphics.draw(self.img, self.x, self.y, 0, self.scale, self.scale)
- end
+function clicker:draw()
+    love.graphics.draw(
+        self.img,
+        self.x, self.y,
+        0,
+        self.scale, self.scale,
+        self.width / 2, self.height / 2
+    )
+end
 
- return clicker
+return clicker
