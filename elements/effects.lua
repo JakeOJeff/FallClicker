e78538dd0dd51ccb1f8acaa74795a43c4d994f51
Paulyn <realjakeojeff@gmail.com>
2025-10-24 18:38:18 +0530

    Succesfully made an effects function

--- elements/effects.lua
+++ elements/effects.lua
@@ -6,6 +6,10 @@ function effects:new()
     local obj = {}
     setmetatable(obj, effects)
 
+    obj.x = love.math.random(0, wW)
+    obj.y = love.math.random(0, wH)
+    obj.rotation = love.math.random()  * 2 * math.pi
+    obj.img = love.graphics.newImage("assets/clicker.png")
 
     return obj
 
