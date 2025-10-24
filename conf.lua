4f88c4c5dc8ebc91f3ff5f7e78ebe5712855152c
Paulyn <realjakeojeff@gmail.com>
2025-10-24 18:33:58 +0530

    Rescaled Images into 2

--- conf.lua
+++ conf.lua
@@ -1,4 +1,4 @@
 function love.conf(t)
-    t.window.width = 576 
-    t.window.height = 324 
+    t.window.width = 576 * 2
+    t.window.height = 324 * 2 
 end
---
+++ elements/effects.lua
@@ -0,0 +1,3 @@
+local effects = {}
+
+return effects
