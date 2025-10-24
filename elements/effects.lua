29a9895d1a371afd5faaf1af4478367e95ce2cc3
Paulyn <realjakeojeff@gmail.com>
2025-10-24 18:42:31 +0530

    Creating an Update function for effects

--- elements/effects.lua
+++ elements/effects.lua
@@ -10,10 +10,32 @@ function effects:new()
     obj.y = love.math.random(0, wH)
     obj.rotation = love.math.random()  * 2 * math.pi
     obj.img = love.graphics.newImage("assets/clicker.png")
+    obj.timer = 0
 
     return obj
 
 end
 
 
+function effects:update(dt)
+    obj.timer = obj.timer + dt
+
+    if obj.rotation < 2 * math.pi then
+        obj.rotation = obj.rotation + 5 * dt
+    end
+
+    if obj.y > wH then
+        table.remove(effects, )
+    end
+
+    if obj.y < wH + obj.img:getHeight() then
+        obj.y = obj.y + 200 * dt
+    end
+
+
+end
+
+function effects:draw()
+    
+end
 return effects
