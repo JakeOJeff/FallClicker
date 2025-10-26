671d3968b3b88d242bddf77c6d1fd0bea7ec597f
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-26 18:20:17 +0530

    Created an update for buttons

--- elements/button.lua
+++ elements/button.lua
@@ -2,34 +2,40 @@ local button = {}
 button.__index = button
 button.items = {}
 
-function button:new()
+function button:new(x, y, img, func, scale)
     obj = setmetatable({}, button)
     
     obj.x = x
     obj.y = y 
     obj.img = img
     obj.width = img:getWidth()
-    obj.height = height:getHeight()
+    obj.height = img:getHeight()
     obj.func = func 
+
+    obj.hovered = false
+    obj.scale = scale
     table.insert(self.items, obj)
     return obj
 
 end
 
 function button:update(dt)
-        if mx > self.sidebarButton.x and mx < self.sidebarButton.x + self.sidebarButton.img:getWidth() and
-       my > self.sidebarButton.y and my < self.sidebarButton.y + self.sidebarButton.img:getHeight() then
-        self.sidebarButton.hovered = true
-       else
-        self.sidebarButton.hovered = false
+    local mx, my = love.mouse.getPosition()
+
+    for i, v in ipairs(button.items) do
+        if mx > v.x and mx < v.x + v.width and my > v.y and my < v.y + v.height then
+            v.hovered = true
+        else
+            v.hovered = false
+        end
+
+        if v.hovered then
+            v.scale = math.min(v.scale + 2 * dt, 1.1)
+        else
+            v.scale = math.max(v.scale - 2 * dt, 1)
+        end
     end
 
-    if self.sidebarButton.hovered then
-        self.sidebarButton.scale = math.min(self.sidebarButton.scale + 2 * dt, 1.1)
-    else
-        self.sidebarButton.scale = math.max(self.sidebarButton.scale - 2 * dt, 1)
-    end
-end
 end
 
 function button:draw()
