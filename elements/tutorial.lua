133476c1e65999ef99250d08ca415e4f1b7cb55a
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-27 04:58:02 +0530

    Draw tutorial

--- elements/tutorial.lua
+++ elements/tutorial.lua
@@ -3,9 +3,26 @@ local tutorial = {}
 function tutorial:load()
     self.items = {
         {
-            condition = {}
+            condition = function ()
+                return true
+            end,
+            done = false,
+            draw = function ()
+                local w = wW/1.5
+                local h = wH/1.2
+                love.graphics.rectangle("fill", wW/2 - w/2, wH/2 - h/2, w, h)
+            end
         }
     }
+end
+
+function tutorial:draw()
+    for i, v in ipairs(self.items) do
+        if not v.done then
+            v.draw()
+            break
+        end
+    end
 end
 
 
