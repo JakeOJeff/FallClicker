8d35db1e9248ce5625f52cb67cbbc3683da07a1d
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2025-10-27 06:29:39 +0530

    Added mousepressed with function

--- elements/tutorial.lua
+++ elements/tutorial.lua
@@ -3,46 +3,46 @@ local tutorial = {}
 function tutorial:load()
     self.cooldown = 0
     self.cooldownTime = 0.3 -- seconds between clicks
-    self.wasMouseDown = false
     self.done = false
 
     self.items = {
         {
-            condition = function ()
+            condition = function()
                 return tutorial:checkClick()
             end,
             done = true,
-            draw = function ()
+            draw = function()
                 local w = wW / 2.5
                 local h = wH / 2.5
-                local text = "Welcome to FallClicker! In this quirky clicker game, your goal isn’t just to earn coins, it’s to level up entire maps! Tap to collect, upgrade your environment, and watch as your world evolves with every click. Unlock new areas, boost your earnings, and see how far your fall can go!"
+                local text =
+                "Welcome to FallClicker! In this quirky clicker game, your goal isn’t just to earn coins, it’s to level up entire maps! Tap to collect, upgrade your environment, and watch as your world evolves with every click. Unlock new areas, boost your earnings, and see how far your fall can go!"
 
                 love.graphics.setColor(0.3, 0.3, 0.3)
-                love.graphics.rectangle("fill", wW/2 - w/2, wH/2 - h/2, w, h, 10, 10)
+                love.graphics.rectangle("fill", wW / 2 - w / 2, wH / 2 - h / 2, w, h, 10, 10)
 
                 love.graphics.setColor(0.8, 0.8, 0.8)
-                love.graphics.rectangle("line", wW/2 - w/2 + 10, wH/2 - h/2 + 10, w - 20, h - 20, 10, 10)
+                love.graphics.rectangle("line", wW / 2 - w / 2 + 10, wH / 2 - h / 2 + 10, w - 20, h - 20, 10, 10)
 
                 love.graphics.setFont(fontSMS)
                 love.graphics.setColor(1, 1, 1)
 
-                local textY = wH/2 - h/4 - 20
-                love.graphics.printf(text, wW/2 - w/2 + 20, textY, w - 40, "center")
+                local textY = wH / 2 - h / 4 - 20
+                love.graphics.printf(text, wW / 2 - w / 2 + 20, textY, w - 40, "center")
 
                 textY = textY + fontSMS:getHeight() + 140
                 love.graphics.setColor(0.8, 0.8, 0.8)
-                love.graphics.printf("'click' to continue tutorial", wW/2 - w/2 + 20, textY, w - 40, "center")
+                love.graphics.printf("'click' to continue tutorial", wW / 2 - w / 2 + 20, textY, w - 40, "center")
             end
         },
         {
-            condition = function ()
+            condition = function()
                 return tutorial:checkClick()
             end,
-            postFunc = function ()
+            postFunc = function()
                 self.done = true
             end,
             done = true,
-            draw = function ()
+            draw = function()
                 local w = 300
                 local h = 100
                 local text = "You can check your Coins over here!"
@@ -56,14 +56,15 @@ function tutorial:load()
 
                 -- Draw Rectangle
                 love.graphics.setLineWidth(5)
-                                love.graphics.setColor(0.3, 0.3, 0.3)
-                love.graphics.rectangle("line", gui.coinDisplay.x - 5, gui.coinDisplay.y - 5, gui.coinDisplay.img:getWidth() + 10, gui.coinDisplay.img:getHeight() + 10, 10, 10)
+                love.graphics.setColor(0.3, 0.3, 0.3)
+                love.graphics.rectangle("line", gui.coinDisplay.x - 5, gui.coinDisplay.y - 5,
+                    gui.coinDisplay.img:getWidth() + 10, gui.coinDisplay.img:getHeight() + 10, 10, 10)
 
                 love.graphics.setFont(fontSMS)
                 love.graphics.setColor(1, 1, 1)
 
-                
-                
+
+
 
                 local textY = 90
                 love.graphics.printf(text, 120, textY, w - 40, "center")
@@ -74,11 +75,11 @@ function tutorial:load()
             end
         },
         {
-            condition = function ()
+            condition = function()
                 return Coins >= 20
             end,
             done = true,
-            draw = function ()
+            draw = function()
                 local w = 300
                 local h = 100
                 local text = "Click the maple leaf till you get to 20 Coins!"
@@ -101,19 +102,17 @@ function tutorial:load()
                 love.graphics.setFont(fontSMS)
                 love.graphics.setColor(1, 1, 1)
 
-                
+
                 local textY = y + 30
                 love.graphics.printf(text, x + 20, textY, w - 40, "center")
-
-
             end
         },
         {
-            condition = function ()
+            condition = function()
                 return gui.sidebarButton.opened
             end,
             done = false,
-            draw = function ()
+            draw = function()
                 local w = 300
                 local h = 100
                 local text = "Open the Upgrades button by clicking on it"
@@ -131,27 +130,26 @@ function tutorial:load()
                 -- Draw Rectangle
                 love.graphics.setLineWidth(5)
                 love.graphics.setColor(0.3, 0.3, 0.3)
-                love.graphics.rectangle("line", gui.sidebarButton.x - 5, gui.sidebarButton.y - 5, gui.sidebarButton.img:getWidth() + 10, gui.sidebarButton.img:getHeight() + 10, 10, 10)
+                love.graphics.rectangle("line", gui.sidebarButton.x - 5, gui.sidebarButton.y - 5,
+                    gui.sidebarButton.img:getWidth() + 10, gui.sidebarButton.img:getHeight() + 10, 10, 10)
 
                 love.graphics.setFont(fontSMS)
                 love.graphics.setColor(1, 1, 1)
 
-                
+
                 local textY = y + 30
                 love.graphics.printf(text, x + 20, textY, w - 40, "center")
-
-
             end
         },
         {
-            condition = function ()
+            condition = function()
                 return background.state >= 2
             end,
-            postFunc = function ()
+            postFunc = function()
                 self.cooldown = 1
             end,
             done = false,
-            draw = function ()
+            draw = function()
                 local w = 300
                 local h = 100
                 local text = "Now click 'Upgrade' to upgrade your clicks and to add content!"
@@ -169,24 +167,24 @@ function tutorial:load()
                 -- Draw Rectangle
                 love.graphics.setLineWidth(5)
                 love.graphics.setColor(0.3, 0.3, 0.3)
-                love.graphics.rectangle("line", gui.sidebar.elements[1].button.x - 5, gui.sidebar.elements[1].button.y - 5, gui.sidebar.elements[1].button.img:getWidth() + 10, gui.sidebar.elements[1].button.img:getHeight() + 10, 10, 10)
+                love.graphics.rectangle("line", gui.sidebar.elements[1].button.x - 5,
+                    gui.sidebar.elements[1].button.y - 5, gui.sidebar.elements[1].button.img:getWidth() + 10,
+                    gui.sidebar.elements[1].button.img:getHeight() + 10, 10, 10)
 
                 love.graphics.setFont(fontSMS)
                 love.graphics.setColor(1, 1, 1)
 
-                
+
                 local textY = y + 20
                 love.graphics.printf(text, x + 20, textY, w - 40, "center")
-
-
             end
         },
         {
-            condition = function ()
+            condition = function()
                 return tutorial:checkClick()
             end,
             done = false,
-            draw = function ()
+            draw = function()
                 local w = 300
                 local h = 100
                 local text = "Once a map is fully upgraded, you can buy the next map!"
@@ -204,16 +202,16 @@ function tutorial:load()
                 -- Draw Rectangle
                 love.graphics.setLineWidth(5)
                 love.graphics.setColor(0.3, 0.3, 0.3)
-                love.graphics.rectangle("line", gui.sidebar.elements[2].button.x - 5, gui.sidebar.elements[2].button.y - 5, gui.sidebar.elements[2].button.img:getWidth() + 10, gui.sidebar.elements[2].button.img:getHeight() + 10, 10, 10)
+                love.graphics.rectangle("line", gui.sidebar.elements[2].button.x - 5,
+                    gui.sidebar.elements[2].button.y - 5, gui.sidebar.elements[2].button.img:getWidth() + 10,
+                    gui.sidebar.elements[2].button.img:getHeight() + 10, 10, 10)
 
                 love.graphics.setFont(fontSMS)
                 love.graphics.setColor(1, 1, 1)
 
-                
+
                 local textY = y + 20
                 love.graphics.printf(text, x + 20, textY, w - 40, "center")
-
-
             end
         }
     }
@@ -225,28 +223,50 @@ function tutorial:update(dt)
     end
 
     for i, v in ipairs(self.items) do
-        if not v.done and v.condition() then
+        if not v.done and type(v.condition) == "function" and v.condition() then
             v.done = true
-            if v.postFunc then
-                v.postFunc()
-            end
+            if v.postFunc then v.postFunc() end
             break
         end
     end
 end
 
+
 function tutorial:checkClick()
-    if self.cooldown <= 0 and love.mouse.isDown(1) then
-        self.cooldown = self.cooldownTime
-        return true
+    local isDown = love.mouse.isDown(1)
+    local clicked = false
+
+    if self.cooldown <= 0 then
+        if isDown and not self.wasMouseDown then
+            -- Mouse was just pressed
+            self.cooldown = self.cooldownTime
+            clicked = true
+        end
     end
-    return false
+
+    self.wasMouseDown = isDown
+    return clicked
 end
 
 function tutorial:draw()
     for i, v in ipairs(self.items) do
         if not v.done then
             v.draw()
+            break
+        end
+    end
+end
+
+function tutorial:mousepressed(x, y, button)
+    if button ~= 1 or self.cooldown > 0 then return end
+
+    for i, v in ipairs(self.items) do
+        if not v.done then
+            if v.condition == "click" then
+                v.done = true
+                if v.postFunc then v.postFunc() end
+                self.cooldown = self.cooldownTime
+            end
             break
         end
     end
